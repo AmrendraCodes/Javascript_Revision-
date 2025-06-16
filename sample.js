a3a01fec2f721e2967319f701ba861d2ef7ae806
Amrendra Kumar <Amrendra1999official@gmail.com>
2025-06-16 17:46:06 +0000

    file commited
    All file updated

--- sample.js
+++ sample.js
@@ -1,6 +1,19 @@
-var x = 5;
-var y = 10;
-var z =  x + y;
+// JS variables
+// var x = 5;
+// var y = 10;
+// var z =  x + y;
 // console.log(z);
 
+// Variables declared with let have Block Scope
 
+// Variables declared with let must be Declared before use
+
+// Variables declared with let cannot be Redeclared in the same scope
+
+// {
+//   let x = 2;
+// }
+// console.log(x);
+// Const using Array
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits[0]);
