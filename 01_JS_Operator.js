89819d10a006190b3af6bc876ed97554a08d1552
amrendra1998 <Amrendra1999official@gmail.com>
2025-06-17 00:02:49 +0530

    file commited

--- 01_JS_Operator.js
+++ 01_JS_Operator.js
@@ -6,9 +6,9 @@
 let text1 = "A";
 let text2 = "B";
 
-text3 = text1 > text2;
+let text3 = text1 > text2;
 
-console.log(text3);
+// console.log(text3);
 
 // JS Logical Operator 
 // &&	logical and
@@ -17,3 +17,37 @@ console.log(text3);
 
 // const a = ""
 // console.log(typeof(a));
+//  Increment Operator (++)
+// Decrement Operator (--)
+
+let x = 5;
+let z = x ** 2;
+// console.log(z);
+//  Operator Precedence 
+
+var p = 100 + 50 * 3;
+// console.log(p);
+
+//  JS Data Types 
+// String
+// Number
+// Bigint
+// Boolean
+// Undefined
+// Null
+// Symbol
+// Object
+
+const length = 10;
+// console.log(typeof(length));
+
+const name = "sonu Kumar";
+console.log(typeof(name));
+
+const employee = {
+    name: "jenil",
+    Id: 123,
+    mob_no: "620548XXXX"
+};
+
+console.log(typeof employee.name);
