8c6ef5e1ccda0818085b1b34bedf31ae040d9e13
amrendra1998 <Amrendra1999official@gmail.com>
2025-06-16 23:41:46 +0530

    file commited

---
+++ 01_JS_Operator.js
@@ -0,0 +1,19 @@
+// JS Opeator 
+// let x = 5;
+// x += 5;
+// console.log(x);
+
+let text1 = "A";
+let text2 = "B";
+
+text3 = text1 > text2;
+
+console.log(text3);
+
+// JS Logical Operator 
+// &&	logical and
+// ||	logical or
+// !	logical not
+
+// const a = ""
+// console.log(typeof(a));
