378d7fc0f379bed7f6cd8b7aaff71d4c4f74a1de
amrendra1998 <Amrendra1999official@gmail.com>
2025-06-18 23:18:42 +0530

    file updated

---
+++ 02_JS_Function.js
@@ -0,0 +1,40 @@
+//  JS Function 
+const var1 = myFunction(25, 25 );
+
+function myFunction(a,b) {
+    return a*b;
+
+}
+console.log(var1);
+
+// Practice 1 ( Add a number)
+
+function add_number(a,b) {
+    return a + b;
+
+}
+console.log(add_number(2,3));
+
+// Practice 2(check even or odd)
+
+function check_even_odd(num) {
+    if(num%2 == 0) {
+       return "value is even"
+    } else {
+        return "value is odd"
+    }
+}
+
+console.log(check_even_odd(2));
+console.log(check_even_odd(3));
+
+// JS Object 
+
+
+
+
+
+
+
+
+
