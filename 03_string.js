27cadfe2dc565ae480c2771966497b1d7a292bd7
amrendra1998 <Amrendra1999official@gmail.com>
2025-06-20 00:00:08 +0530

    file updated

---
+++ 03_string.js
@@ -0,0 +1,42 @@
+// console.log("Amrendra k'umar ")
+// string 
+let x = "ram"
+let y = new String ("John");
+
+// console.log(typeof(x));
+// console.log(typeofy);
+
+// const studentName = "ShyamPrasad"
+// console.log(studentName.toUpperCase());
+
+const str = "apple,bananan,guava"
+const arr = str.split(",");
+
+// console.log(arr);
+
+console.log(`Hello world`);
+
+
+//  JS Array 
+
+let fruits = ["Banana","Apple","Pinepapple","Guava","Grapes"]
+// console.log(fruits);
+
+const number = new Array(1,2,3,4);
+// console.log(number);
+// console.log(number[1]);
+
+console.log(fruits);
+fruits[1] = "pencil"
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits.push("rock"))
+console.log(fruits);
+
+
+
+
+
+
+
+
